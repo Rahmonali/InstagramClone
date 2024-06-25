dc3b7ccc1bfefbe6ca25a6f7b047e38b0a6de2c6
Rahmonali Fatkhuddinov <rahmonali1995@gmail.com>
2024-06-25 10:09:08 +0500

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Rahmonali on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
